625ff1c498791eaaaa467b8a60136c36a51f3b14
Raphaël Méline <raphael.meline@bordeaux-inp.fr>
2019-04-02 15:12:21 +0200

    ajout du programme gérant la grille de jeu sous forme de bitboard

--- src/bitboard.c
+++ src/bitboard.c
@@ -5,20 +5,36 @@
 #include <stddef.h>
 // the board is to be read from top to bottom and from left to right starting from the lightest bit to the heaviest one
 
-struct bitboard new_bitboard(){
+struct bitboard new_bitboard(){ //creates a new empty bitboard for black and white
   struct bitboard b;
   b.white=0;
   b.black=0;
   return b;
 }
 
-int move_played (struct col_move_t *move_done, struct bitboard *board, size_t size){
+int play_move (struct col_move_t *move_done, struct bitboard *board, size_t size){ //plays a move
     int cell_number= move_done->m.row*size+move_done->m.col;
     if (move_done->c==WHITE)
         board->white=board->white | (1<<cell_number);
     if (move_done->c==BLACK)
         board->black=board->black | (1<<cell_number);
     return 0;
+}
+__uint128_t bitwise_and(__uint128_t a, __uint128_t b){
+  __uint128_t res= 0;
+  unsigned long long c=(unsigned long long) a;
+  unsigned long long d=(unsigned long long) b;
+  unsigned long long e=c&d;
+  unsigned long long f=(unsigned long long) (a>>64);
+  unsigned long long g=(unsigned long long) (b>>64);
+  unsigned long long h=f&g;
+  res= h;
+  res<<=64;
+  res+=e;
+}
+
+int pattern_is_matching(__uint128_t pattern, __uint128_t colored_board){
+    return ( bitwise_and(pattern, colored_board)== pattern );
 }
 
 int color_is_winning(__uint128_t colored_board, size_t size, size_t winning_threshold){ //winning_threshold is 5 in the classical rules
@@ -28,39 +44,76 @@ int color_is_winning(__uint128_t colored_board, size_t size, size_t winning_thre
     __uint128_t pattern_vert=0;
     for (size_t k=0; k<winning_threshold; k++){
         pattern_diag_down += 1<<(k*(size+1));
-        pattern_diag_up += (1<<(size-1))<<(k*(size-1));
+        pattern_diag_up += (1<<(winning_threshold-1)) <<(k*(size-1));
         pattern_vert += 1<<(k*size);
         pattern_horiz += 1<<k;
     }
     for (size_t i=0; i<size-winning_threshold+1; i++) {
         for (size_t j=0; j<size-winning_threshold+1; j++) {
-            if ( ( (pattern_diag_up & colored_board)== pattern_diag_up) || ( (pattern_diag_down & colored_board) == pattern_diag_down) )
-                return 1;
-            pattern_diag_down<<=1;
-            pattern_diag_up>>=1;
-      }
-        pattern_diag_up>>=(winning_threshold-1);
-        pattern_diag_down<<=(winning_threshold-1);
+            if (   (  ( (i==size-winning_threshold) && (j==size-winning_threshold) || (i==0) && (j==0) ) //upward diagonal being stuck in a corner there are no overlines possible
+                && ( pattern_is_matching(pattern_diag_up, colored_board) )  )
+            || (  ( (i==0) || (j==size-winning_threshold) ) //upward diagonal touches left or bottom border
+                && ( pattern_is_matching(pattern_diag_up, colored_board) && pattern_is_matching(pattern_diag_up>>(size-1), colored_board)==0 )  )
+            || (  ( (i== size-winning_threshold) || (j==0) ) //upward diagonal touches right or top border
+                && ( pattern_is_matching(pattern_diag_up, colored_board) && pattern_is_matching(pattern_diag_up<<(size-1), colored_board)==0 )  )
+            || ( pattern_is_matching(pattern_diag_up, colored_board) && pattern_is_matching(pattern_diag_up<<(size-1), colored_board)==0  //upward diagonal doesn't touch any corner: overlines are possibles both ways
+                && pattern_is_matching(pattern_diag_up>>(size-1), colored_board)==0 ) )
+                    return 1;
+            pattern_diag_up<<=1;
+
+        }
+        pattern_diag_up<<=(winning_threshold-1);
     }
     for (size_t i=0; i<size; i++) {
         for (size_t j=0; j<size-winning_threshold+1; j++) {
-            if ((pattern_vert & colored_board ) == pattern_vert)
+            if (  ( (i==0) //vertical line touches top border
+                    && ( pattern_is_matching(pattern_vert, colored_board) && pattern_is_matching(pattern_vert<<size, colored_board)==0 )  )
+                || ( (i==size-winning_threshold) //vertical line touches bottom border
+                    && ( pattern_is_matching(pattern_vert, colored_board) && pattern_is_matching(pattern_vert>>size, colored_board)==0 )  )
+                || ( pattern_is_matching(pattern_vert,colored_board) && pattern_is_matching(pattern_vert>>size, colored_board)==0
+                    && pattern_is_matching(pattern_vert<<size, colored_board)==0 )  )//vertical line doesn't touch any border
                 return 1;
-            pattern_vert<<=1;
+                pattern_vert<<=1;
         }
     }
-    for (size_t i=0; i<size-winning_threshold+1; i++) {
-        for (size_t j=0; j<size; j++) {
-            if ((pattern_horiz & colored_board) == pattern_horiz)
+    for (size_t i=0; i<size; i++) {
+        for (size_t j=0; j<size-winning_threshold+1; j++) {
+            if (  ( (j==0) //horizontal line touches left border
+                    && ( pattern_is_matching(pattern_horiz, colored_board) && pattern_is_matching(pattern_horiz<<1, colored_board)==0 )  )
+                || ( (j==size-winning_threshold) //horizontal line touches right border
+                    && ( pattern_is_matching(pattern_horiz, colored_board) && pattern_is_matching(pattern_horiz>>1, colored_board)==0 )  )
+                || ( pattern_is_matching(pattern_horiz,colored_board) && pattern_is_matching(pattern_horiz>>1, colored_board)==0
+                    && pattern_is_matching(pattern_horiz<<1, colored_board)==0 )  )//horizontal line doesn't touch any border
                 return 1;
-            pattern_horiz<<=1;
+                pattern_horiz<<=1;
         }
-        pattern_horiz<<=winning_threshold-1;
+        pattern_horiz<<=(winning_threshold-1);
+    }
+    for (size_t i=0; i<size-winning_threshold+1; i++) {
+        for (size_t j=0; j<size-winning_threshold+1; j++) {
+            if (   (  ( (i==0) && (j==size-winning_threshold) || (i==size-winning_threshold) && (j==0) ) //downward diagonal being stuck in a corner there are no overlines possible
+                && ( pattern_is_matching(pattern_diag_down, colored_board) )  )
+            || (  ( (i==0) || (j==0) ) //downward diagonal touches left or top border
+                && ( pattern_is_matching(pattern_diag_down, colored_board) && pattern_is_matching(pattern_diag_down<<(size+1), colored_board)==0 )  )
+            || (  ( (i==size-winning_threshold) || (j==0) ) //downward diagonal touches right or bottom border
+                && ( pattern_is_matching(pattern_diag_down, colored_board) && pattern_is_matching(pattern_diag_up>>(size+1), colored_board)==0 )  )
+            || ( pattern_is_matching(pattern_diag_down, colored_board) && pattern_is_matching(pattern_diag_up>>(size+1), colored_board)==0  //downward diagonal doesn't touch any corner: overlines are possibles both ways
+                && pattern_is_matching(pattern_diag_down>>(size+1), colored_board)==0 ) )
+                    return 1;
+            pattern_diag_down<<=1;
+
+        }
+        pattern_diag_down<<=(winning_threshold-1);
     }
     return 0;
   }
-  //TODO vérifier que la ligne est de longueur au plus 5
-  int main(){
-    printf ("%d\n" ,color_is_winning(1+32+32*32+32*32*32+32*32*32*32, 5, 5));
+  //TODO vérifier que la ligne est de longueur au plus 5 DONE
+  /*int main_test(){
+    __uint128_t x= 1+64+64*64+64*64*64+64*64*64*64;
+    __uint128_t y= 3+64+64*64+64*64*64+64*64*64*64;
+    //x+=64*64*64*64*64;
+    printf("%llu\n", bitwise_and(x, y) );
+    printf("%d\n", pattern_is_matching( x,y));
+    printf ("%d\n" ,color_is_winning(x , 6, 5));
         return 0;
-  }
+  }*/
